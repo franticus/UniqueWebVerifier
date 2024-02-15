296ceeec0bba14e800001c6e90c5734c4b081952
S.Zelensky <zelenstein@gmail.com>
2024-02-15 14:32:59 +0300

    plain

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const multer = require('multer');
 const AdmZip = require('adm-zip');
-const { JSDOM } = require('jsdom');
-const crypto = require('crypto');
 const fs = require('fs');
 const path = require('path');
 const app = express();
 const port = 3001;
 
-// Убедитесь, что директория 'uploads' существует
 const uploadDir = path.join(__dirname, 'uploads');
+const checkedArchiveDir = path.join(__dirname, 'checkedArchive');
+
+// Убедимся, что директории существуют
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
+}
+if (!fs.existsSync(checkedArchiveDir)) {
+  fs.mkdirSync(checkedArchiveDir);
 }
 
 const upload = multer({ dest: uploadDir });
@@ -24,51 +27,39 @@ app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-app.post('/upload', upload.single('siteZip'), (req, res) => {
+app.post('/upload', upload.single('siteZip'), async (req, res) => {
   if (req.file) {
     console.log('Uploaded: ', req.file.path);
-    unpackAndAnalyzeArchive(req.file.path)
-      .then(() =>
-        res.send(`File uploaded successfully: ${req.file.originalname}`)
-      )
-      .catch(error => {
-        console.error('Error processing the file', error);
-        res.status(500).send('Error processing the file');
-      });
+    await unpackAndSavePlainText(req.file.path);
+    res.send(`File uploaded successfully: ${req.file.originalname}`);
   } else {
     res.status(400).send('No file uploaded.');
   }
 });
 
-async function unpackAndAnalyzeArchive(filePath) {
+async function unpackAndSavePlainText(filePath) {
   const zip = new AdmZip(filePath);
   const zipEntries = zip.getEntries();
 
   for (const entry of zipEntries) {
     if (entry.entryName.endsWith('.html')) {
       const content = zip.readAsText(entry);
-      const text = extractTextFromHtml(content);
-      await analyzeAndCompareHash(entry.entryName, text); // Обеспечиваем асинхронную обработку
+      const plainText = stripTags(content);
+      const fileName = path.basename(entry.entryName, '.html') + '.txt';
+      const outputPath = path.join(checkedArchiveDir, fileName);
+      fs.writeFileSync(outputPath, plainText);
+      console.log(`PlainText for ${entry.entryName} saved to ${outputPath}`);
     }
   }
+
+  // Очистка папки uploads после обработки файла
+  fs.unlinkSync(filePath);
 }
 
-function analyzeAndCompareHash(entryName, text) {
-  try {
-    const hash = generateHash(text);
-    const hashes = readHashes();
-
-    if (hash in hashes) {
-      console.log(
-        `Содержимое ${entryName} не уникально. Найдено совпадение: ${hashes[hash]}`
-      );
-    } else {
-      console.log(`Содержимое ${entryName} уникально и сохранено.`);
-      hashes[hash] = entryName;
-      saveHashes(hashes);
-    }
-  } catch (error) {
-    console.error('Ошибка при анализе и сравнении хеша:', error);
-    throw error; // Перебрасываем ошибку для последующей обработки
-  }
+function stripTags(html) {
+  // Удаляем HTML теги
+  let plainText = html.replace(/<[^>]*>?/gm, '');
+  // Удаляем пустые строки и лишние пробелы
+  plainText = plainText.replace(/\s+/g, ' ').trim(); // Удаляем лишние пробелы и обрезаем пробелы в начале и конце строки
+  return plainText;
 }
